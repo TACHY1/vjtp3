e841178ae225b31b6c3174c0df41908cdd39cf90
guillermo liss <lissguillermo@gmail.com>
2012-10-27 19:24:24 -0300

    creando dungeon generator parte 1

---
+++ HighAndDangerous/Assets/Scripts/DungeonCreator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DungeonCreator : MonoBehaviour {
+	
+	public int numChunks;
+	
+	public void Generate() {
+	
+	}
+	
+	
+	public void RemoveAll() {
+	}
+}
